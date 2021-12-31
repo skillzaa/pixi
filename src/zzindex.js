c5dcb28b275b03a6cda4851a458793ec2944e358
skilzaa <skillzaa.com@gmail.com>
2021-12-31 12:34:47 +0500

    Alhamdullah my setup for the lib is ok- do not make it more complicated- it has to be 2 libs one a static draw function on json and other the puppetter

--- src/zzindex.js
+++ src/zzindex.js
@@ -1,5 +1,4 @@
 
 let de = new DrawEngine();
-let app = de.get_app();
-let yellow = de.rectangle(app,100,20,300,300,0xFFFF00);
-app.stage.addChild(yellow);
+let yellow = de.rectangle(100,20,300,300,0xFFFF00);
+// app.stage.addChild(yellow);
